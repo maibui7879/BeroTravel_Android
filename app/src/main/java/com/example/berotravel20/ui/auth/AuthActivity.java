498c6e1b977f19df818f5f225c17d1411cfc4dc5
huyendieu0201 <nguyenthihuyendieu0201@gmail.com>
2025-12-06 00:07:34 +0700

    create dieu branch

--- app/src/main/java/com/example/berotravel20/ui/auth/AuthActivity.java
+++ app/src/main/java/com/example/berotravel20/ui/auth/AuthActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.example.berotravel20.R;
 
-//Đây là test api. khi làm nhớ sửa lại
-
 public class AuthActivity extends AppCompatActivity {
 
     @Override
@@ -14,19 +12,15 @@ public class AuthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        // Mặc định load LoginFragment khi mở màn hình này
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.auth_container, new com.example.berotravel20.ui.auth.LoginFragment())
-                    .commit();
+            loadFragment(new LoginFragment());
         }
     }
 
-    // Hàm public để Fragment con có thể gọi khi muốn chuyển trang
-    public void switchFragment(Fragment fragment) {
+    public void loadFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.auth_container, fragment)
-                .addToBackStack(null) // Cho phép bấm Back để quay lại
+                .addToBackStack(null)
                 .commit();
     }
 }
---
+++ app/src/main/java/com/example/berotravel20/ui/main/map/MapFragment.java
@@ -0,0 +1,43 @@
+package com.example.berotravel20.ui.main.map;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.berotravel20.R;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap map) {
+        googleMap = map;
+    }
+}
